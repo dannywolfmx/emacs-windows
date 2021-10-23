c37b824f2a5b021fb4e5fef43409e0df3875f9ac
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2021-10-23 10:38:24 -0500

    First commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(require 'org)
+(org-babel-load-file
+  (expand-file-name "settings.org"
+     user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Documents/proveedores.org"))
+ '(package-selected-packages
+   '(undo-tree use-package-ensure-system-package evil doom-themes delight)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
